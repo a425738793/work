15383694a54fc9521f2a9a0b2ead6ecfb3e26113
Your Name <you@example.com>
2017-10-24 19:37:28 +0800

    笔记

--- my-project/src/components/vuex.vue
+++ my-project/src/components/vuex.vue
@@ -3,10 +3,12 @@
 		<input type="button" value="-" @click="minNum(2,'zhangsan',20)" />
 		{{num}}
 		<input type="button" value="+" @click="addNum(5,'lisi',18)" />
+		{{num2}}
 	</div>
 </template>
 
 <script>
+	import {mapState,mapGetters,mapActions} from 'vuex'
 	export default {
 //	  name: 'Vuex',
 //	  data:function () {
@@ -15,26 +17,65 @@
 //	  		num : this.$store.state.num
 //	  	}
 //	  },
-	  computed:{
-            num(){
-              return  this.$store.state.num
-            }
-      },
+//	  computed:{
+//          num(){
+//            return  this.$store.state.num
+//          },
+//          num2(){
+//          	return this.$store.getters.count
+//          }
+//    },
+//	  computed: mapState(['num']),
+//	  computed: mapState({
+//	  	num: state=> state.num  //写法1
+//		num: 'num'              //写法2
+//		num(state){             //写法3
+//			return state.num+100
+//		}
+//	  }),
+	  computed: {
+	  	...mapState({
+	  		num: 'num'
+	  	}),
+	  	...mapGetters({
+	  		count: 'count'
+	  	}),
+	  	num2(){
+	  		return this.$store.getters.count
+	  	}
+	  },
 	  methods : {
-	  	addNum(num,str,age){
+//	  	addNum(num,str,age){
 //	  		this.num+=5
 //			this.$store.commit('Addnum')
 //			this.$store.commit('Addnum',num)
-			this.$store.commit('Addnum',{n:num,name:str,ages:age})
-			
-	  	},
+//			this.$store.commit('Addnum',{n:num,name:str,ages:age})
+//			this.$store.commit({
+//				type: 'Addnum',
+//				n: num,
+//				name: str,
+//				ages: age
+//			})
+//			this.$store.dispatch('addAction',{n:num,name:str,ages:age})
+//	  	},
 	  	minNum(num,str,age){
 //	  		this.num-=5
 //	  		this.$store.commit('Minnum')
 //	  		this.$store.commit('Minnum',num)
 	  		this.$store.commit('Minnum',{n:num,name:str,ages:age})
 	  		
-	  	}
+	  	},
+	  	...mapActions({
+	  		addNum:'addAction'
+//	  		addNum:{
+//	  			type: 'addAction',
+//	  			n: 5,
+//	  			name: 'wangwu',
+//	  			ages: 20
+//	  		},
+//	  		
+		
+	  	})
 	  }
 	}
 </script>
--- my-project/src/store/index.js
+++ my-project/src/store/index.js
@@ -7,9 +7,18 @@ let store = new Vuex.Store({
 	state: {
 		num: 100
 	},
+	getters: {
+		count: state=>{
+			return state.num > 130 ? 130 :state.num
+		}
+	},
 	mutations: {
 		Addnum (state,s) {
 //			state.num+=num
+//			如果写异步的话状态会发生混乱 mutations 的原则是同步的
+//			setTimeout(()=>{
+//				state.num+=s.n
+//			},1000)
 			state.num+=s.n
 			console.log(s)
 			console.log(s.name)
@@ -23,6 +32,21 @@ let store = new Vuex.Store({
 			console.log(num.name)
 			console.log(num.ages)
 			
+		}
+	},
+	actions: {
+		addAction(context,argument){
+			console.log(argument);
+			console.log(context);
+			setTimeout(()=>{
+				
+//				context.commit('Addnum',{n:5,name:'wangwu',ages:20})
+				context.commit('Addnum',{n:5,name:'wangwu',ages:20})
+			},1000)
+			context.dispatch('textAction')
+		},
+		textAction(context){
+			console.log('textActionc触发了')
 		}
 	}
 })
